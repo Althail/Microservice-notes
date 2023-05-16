79ac12205d21953b278a031ac2189b621528e6ac
sangmin <tkdals4723@gmail.com>
2023-05-16 10:35:25 +0200

    Project Started

---
+++ RatingSystem/src/main/java/epsi_b3_g2_rating/RatingSystem/RatingSystemApplication.java
@@ -0,0 +1,13 @@
+package epsi_b3_g2_rating.RatingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingSystemApplication.class, args);
+	}
+
+}
